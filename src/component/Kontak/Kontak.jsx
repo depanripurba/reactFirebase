653867f96c7b09e1520cd0a19f003b178e201783
Depanri Purba <depanri.purba@gmail.com>
2020-05-21 06:51:10 +0700

    memperbagus

--- src/component/Kontak/Kontak.jsx
+++ src/component/Kontak/Kontak.jsx
@@ -1,39 +1,33 @@
-import React,{useState} from 'react'
-import './Kontak.css'
-import {Link} from 'react-router-dom'
-const Kontak = ({kontak})=>{
-    const namaa = ['depanri','antoni purba','minalda purba','feriandoni']
-    const Chat = (e)=>{
-        console.log(e.target.innerHTML)
-        kontak(e.target.innerHTML)
+import React, { useState } from "react";
+import "./Kontak.css";
+import { Link } from "react-router-dom";
+const Kontak = ({ kontak }) => {
+    const namaa = ["depanri", "antoni purba", "minalda purba", "feriandoni"];
+    var link
+    const Chat = (e) => {
+        console.log(e.target.innerHTML);
+        kontak(e.target.innerHTML);
+    };
+    const removeActive = ()=>{
+      
     }
-    return(
+    const Active = function(e){
+        link = e.target
+        console.log(link)
+        link.classList.add('active')
+    }
+    return (
         <div>
-        <div class="list-group">
-        <Link to="/" className="list-group-item list-group-item-action">Depanri Purba</Link>
-           <a href="#" class="list-group-item list-group-item-action active">
-            Cras justo odio
-            </a>
-            <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
-            <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
-            <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
-            <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
-            <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-            <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-       
+            <div className="list-group">
+                <Link to="/" className="list-group-item list-group-item-action" onClick={(e)=>Active(e)}>
+                    Depanri Purba
+                </Link>
+                <Link to="/" className="list-group-item list-group-item-action" onClick={(e)=>Active(e)}>
+                    Depanri Purba
+                </Link>
+            </div>
         </div>
-        </div>
-    )
-}
+    );
+};
 
-export default Kontak
+export default Kontak;
